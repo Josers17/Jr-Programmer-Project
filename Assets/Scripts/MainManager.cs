8523d0630d552a72a17d109997749ce9e058e1f9
Jose Morales <josea.91@hotmail.com>
2025-07-29 14:03:07 -0400

    Modify the Awake method

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -8,6 +8,14 @@ public class MainManager : MonoBehaviour
 
     private void Awake()
     {
+        // start of new code
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        // end of new code
+
         Instance = this;
         DontDestroyOnLoad(gameObject);
     }
