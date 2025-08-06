1cd0c828fbe44e1a481263adaccb951df5d1db08
Jose Morales <josea.91@hotmail.com>
2025-08-06 01:02:38 +0000

    Override the BuildingInRange method

--- Assets/Scripts/ProductivityUnit.cs
+++ Assets/Scripts/ProductivityUnit.cs
@@ -2,8 +2,23 @@ using UnityEngine;
 
 public class ProductivityUnit : Unit
 {
+    // new variables
+    private ResourcePile m_CurrentPile = null;
+    public float ProductivityMultiplier = 2;
+
     protected override void BuildingInRange()
     {
+        // start of new code
+        if (m_CurrentPile == null)
+        {
+            ResourcePile pile = m_Target as ResourcePile;
 
+            if (pile != null)
+            {
+                m_CurrentPile = pile;
+                m_CurrentPile.ProductionSpeed *= ProductivityMultiplier;
+            }
+        }
+        // end of new code
     }
 }
